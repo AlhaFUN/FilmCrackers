a92b3c5737d55aa530a5abfff50a9160a31e5183
AlhaFUN <125183297+AlhaFUN@users.noreply.github.com>
2025-06-26 20:18:27 -0400

    docs: Update help command to include !!info

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,3 @@
-// /commands/help.js
 const { EmbedBuilder } = require('discord.js');
 const { log } = require('../utils/logger');
 
@@ -7,12 +6,12 @@ module.exports = async (message) => {
   const helpEmbed = new EmbedBuilder()
     .setColor('#f1c40f')
     .setTitle('❓ Help & Commands')
-    .setDescription('Here are all the commands you can use:')
+    .setDescription('Here are all the commands you can use with the bot.')
     .addFields(
       {
         name: '👑 Admin Commands',
         value:
-          '`!!autoreact enable` - Activates auto-reactions in the current channel.\n' +
+          '`!!autoreact enable` - Activates auto-reactions in this channel.\n' +
           '`!!autoreact disable` - Deactivates auto-reactions.\n' +
           '`!!autoreact setemojis 😀 😡` - Sets the two emojis for reactions.\n' +
           '`!!dm <userID> <message>` - Sends a direct message to a user.',
@@ -20,6 +19,7 @@ module.exports = async (message) => {
       {
         name: '🌍 Global Commands',
         value:
+          '`!!info <movie/tv name>` - Fetches detailed info about a movie or show. *Tip: Add the year for better accuracy!* \n' +
           '`!!status` - Shows the current status of the bot and its systems.\n' +
           '`!!help` - Displays this help message.',
       }
